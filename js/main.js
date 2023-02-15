bb32b36ca1dba8a481a99bceaded53771af366ad
David <david.melo@lc.cuny.edu>
2023-02-15 18:51:31 -0500

    getWinner and textContent

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
@@ -19,7 +30,7 @@ function render() {
     board.forEach(function(mark, index){
         squares[index].textContent = mark;
     });
-    messages.textContent = `It's ${turn}'s turn!`;
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
 
 /*----- cached element references -----*/
@@ -30,11 +41,22 @@ const messages = document.querySelector('h2');
 document.getElementById('board').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && 
+            board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner;
+    };
+
+
 function handleTurn(event) {
     let idx = squares.findIndex(function(square) {
         return square === event.target;
     });
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
     render();
 };
